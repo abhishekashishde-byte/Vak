a403aaf843800f0e2e0886f7f5b2d08e986444a9
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-11 15:16:28 +0200

    Route Vak through Google auth gate

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import AuthGate from './AuthGate.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode><AuthGate /></React.StrictMode>
 )
