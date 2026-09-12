47c316cb7dcc189750171831627dd98ab42904e9
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-13 01:08:23 +0200

    Enable Ana natural realtime voice

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AuthGate from './AuthGate.jsx'
+import { installAnaNaturalVoice } from './naturalVoice.js'
 import './styles.css'
 import './briefVoice.css'
 import './briefVoice.js'
+
+installAnaNaturalVoice()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><AuthGate /></React.StrictMode>
