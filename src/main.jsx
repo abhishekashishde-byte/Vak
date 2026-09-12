b4857c689356460f7b1ed59a74a67e3279ac147f
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-13 01:46:36 +0200

    Enable automatic counterparty view in Talk for me

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client'
 import AuthGate from './AuthGate.jsx'
 import { installAnaNaturalVoice } from './naturalVoice.js'
 import { installContextualIntelligence } from './contextualIntelligence.js'
+import { installCounterpartyView } from './counterpartyView.js'
 import { installPersonalLanguageMemory } from './personalLanguageMemory.js'
 import { installRealtimeTonePolicy } from './realtimeTone.js'
 import './styles.css'
 import './briefVoice.css'
 import './criticalFacts.css'
+import './counterpartyView.css'
 import './briefVoice.js'
 
 installPersonalLanguageMemory()
 installContextualIntelligence()
 installRealtimeTonePolicy()
+installCounterpartyView()
 installAnaNaturalVoice()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
