bcb9708d957e2971a74a7c38e3e4312c9b499a86
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-13 01:02:53 +0200

    Enable voice briefing input in Ana

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AuthGate from './AuthGate.jsx'
 import './styles.css'
+import './briefVoice.css'
+import './briefVoice.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><AuthGate /></React.StrictMode>
