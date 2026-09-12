b11800fce5968d2847270e6bffd92ee869958429
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-13 01:18:31 +0200

    Enable adaptive realtime tone policy

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AuthGate from './AuthGate.jsx'
 import { installAnaNaturalVoice } from './naturalVoice.js'
+import { installRealtimeTonePolicy } from './realtimeTone.js'
 import './styles.css'
 import './briefVoice.css'
 import './briefVoice.js'
 
+installRealtimeTonePolicy()
 installAnaNaturalVoice()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
