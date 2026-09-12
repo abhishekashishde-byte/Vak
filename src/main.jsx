f6fc40681ea70883c6aae51b2326f82f41d4528d
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-12 08:16:39 +0200

    Remove CSS image override for Ana icon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AuthGate from './AuthGate.jsx'
 import './styles.css'
-import './icon.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><AuthGate /></React.StrictMode>
