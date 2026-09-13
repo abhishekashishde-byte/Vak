398837cc9596b62e186d42be299e0f29e6cd3be1
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-13 08:52:13 +0200

    Enable synced memory and privacy architecture

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AuthGate from './AuthGate.jsx'
+import { installAccountPreferenceSync } from './accountPreferences.js'
 import { installAnaNaturalVoice } from './naturalVoice.js'
 import { installContextualIntelligence } from './contextualIntelligence.js'
+import { installConversationPrivacyGate } from './conversationPrivacyGate.js'
 import { installCounterpartyView } from './counterpartyView.js'
 import { installPersonalLanguageMemory } from './personalLanguageMemory.js'
 import { installPushToTalkFallback } from './pushToTalk.js'
@@ -12,13 +14,16 @@ import './briefVoice.css'
 import './criticalFacts.css'
 import './counterpartyView.css'
 import './pushToTalk.css'
+import './privacySettings.css'
 import './briefVoice.js'
 
+installAccountPreferenceSync()
 installPersonalLanguageMemory()
 installContextualIntelligence()
 installRealtimeTonePolicy()
 installCounterpartyView()
 installPushToTalkFallback()
+installConversationPrivacyGate()
 installAnaNaturalVoice()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
