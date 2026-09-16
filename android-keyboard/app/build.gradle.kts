7ee61d2de86fbd60ba3255c0fc51f2e81ecbc560
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-16 08:22:12 +0200

    Version Ana Keyboard v0.2

--- android-keyboard/app/build.gradle.kts
+++ android-keyboard/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ana.keyboard"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     compileOptions {
