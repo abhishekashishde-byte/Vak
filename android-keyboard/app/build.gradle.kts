40300e060fa8aa0dcde28ba595d5b64cb47b3327
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-22 21:50:50 +0200

    Bump Ana Keyboard to 0.12.1

--- android-keyboard/app/build.gradle.kts
+++ android-keyboard/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ana.keyboard"
         minSdk = 28
         targetSdk = 37
-        versionCode = 12
-        versionName = "0.12.0"
+        versionCode = 13
+        versionName = "0.12.1"
     }
 
     compileOptions {
