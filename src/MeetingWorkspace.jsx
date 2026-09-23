6ba567826f312b62c3333431d5dc85dc0ad591a8
abhishekashishde-byte <abhishek.ashish.de@gmail.com>
2026-09-23 13:51:28 +0200

    Add Action Center to meeting workspace

--- src/MeetingWorkspace.jsx
+++ src/MeetingWorkspace.jsx
@@ -1,9 +1,11 @@
 import MeetingMode from './MeetingMode.jsx'
+import ActionCenter from './ActionCenter.jsx'
 import MeetingIntelligence from './MeetingIntelligence.jsx'
 
 export default function MeetingWorkspace() {
   return <>
     <MeetingMode />
+    <ActionCenter />
     <MeetingIntelligence />
   </>
 }
